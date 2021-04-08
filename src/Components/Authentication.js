cc6b58c31d68681501e31b1afd75d909a3e3faaa
Daniel Masud <danielbmasud@gmail.com>
2021-04-07 23:09:52 -0500

    Created Sign Up button for authentication page

--- src/Components/Authentication.js
+++ src/Components/Authentication.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./Authentication.css";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Button, Form, FormGroup, Label, Input, Container } from "reactstrap";
 
 import {
   FacebookLoginButton,
   GoogleLoginButton,
 } from "react-social-login-buttons";
+import { StylesProvider } from "@material-ui/styles";
 
 class Authentication extends React.Component {
   render() {
     /*
-            The render code was sourced from a youtube tutorial showing how to create a simple yet professional looking login page. All credit to Kris Foster, https://www.youtube.com/watch?v=XHPL-rX9m-Q
-            Some minor tweaks include adding the google login button and instead of writing it in the main App.js file, it is written as a component and will be called when the user clicks on the sign in link
+            The render code was inspired from a youtube tutorial showing how to create a simple yet professional looking login page. All credit to Kris Foster, https://www.youtube.com/watch?v=XHPL-rX9m-Q
+            Some minor tweaks include adding the google login button and instead of writing it in the main App.js file, it is written as a component and will be called when the user clicks on the sign in link. More differences have been added to fit the aesthetic of our website
         */
     return (
       <Form className="Login">
@@ -27,14 +28,24 @@ class Authentication extends React.Component {
           />
         </FormGroup>
 
-        <div className="main-button">
-          <button
-            className="btn-lg btn-dark"
-            onClick={() => this.handleLogIn()}
-          >
-            Log in
-          </button>
-        </div>
+        <Container>
+          <Container className="login-button">
+            <button
+              className="btn-lg btn-dark"
+              onClick={() => this.handleLogIn()}
+            >
+              Log in
+            </button>
+          </Container>
+          <Container className="signup-button">
+            <button
+              className="btn-lg btn-dark"
+              onClick={() => this.handleSignUp()}
+            >
+              Sign Up
+            </button>
+          </Container>
+        </Container>
         <div className="text-center pt-3">
           Or continue with your social account
         </div>
@@ -47,14 +58,15 @@ class Authentication extends React.Component {
           onClick={() => this.handleGoogle()}
         />
         <div className="text-center">
-          <a href="/sign-up">Sign up</a>
-          <span className="p-2">|</span>
           <a href="/forgot-password">Forgot Password</a>
         </div>
       </Form>
     );
   }
 
+  /*
+    The following handlers must be updated
+  */
   handleLogIn() {
     alert("Log in");
   }
@@ -65,6 +77,10 @@ class Authentication extends React.Component {
 
   handleGoogle() {
     alert("Google");
+  }
+
+  handleSignUp() {
+    alert("Signed Up");
   }
   //Login functionality will be replaced in the future
 }
