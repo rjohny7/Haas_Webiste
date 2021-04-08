d838dae164b972d77fe75f7be942047fb0260012
Daniel Masud <danielbmasud@gmail.com>
2021-04-07 22:27:28 -0500

    Cleaned up the login page a bit and added dummy methods for button handlers

--- src/Components/Authentication.js
+++ src/Components/Authentication.js
@@ -10,25 +10,42 @@ import {
 class Authentication extends React.Component {
   render() {
     /*
-            The following code was sourced from a youtube tutorial showing how to create a simple yet professional looking login page. All credit to Kris Foster, https://www.youtube.com/watch?v=XHPL-rX9m-Q
+            The render code was sourced from a youtube tutorial showing how to create a simple yet professional looking login page. All credit to Kris Foster, https://www.youtube.com/watch?v=XHPL-rX9m-Q
             Some minor tweaks include adding the google login button and instead of writing it in the main App.js file, it is written as a component and will be called when the user clicks on the sign in link
         */
     return (
       <Form className="Login">
-        <FormGroup>
-          <Label>Email</Label>
-          <Input type="email" placeholder="Email" />
+        <FormGroup className="col">
+          <Input className="input-email" type="email" placeholder="Email" />
         </FormGroup>
+
         <FormGroup>
-          <Label>Password</Label>
-          <Input type="password" placeholder="Password" />
+          <Input
+            className="input-password"
+            type="password"
+            placeholder="Password"
+          />
         </FormGroup>
-        <Button className="btn-lg btn-dark btn-block">Log in</Button>
+
+        <div className="main-button">
+          <button
+            className="btn-lg btn-dark"
+            onClick={() => this.handleLogIn()}
+          >
+            Log in
+          </button>
+        </div>
         <div className="text-center pt-3">
           Or continue with your social account
         </div>
-        <FacebookLoginButton className="mt-3 mb-3" />
-        <GoogleLoginButton className="mt-3 mb-3" />
+        <FacebookLoginButton
+          className="mt-3 mb-3"
+          onClick={() => this.handleFacebook()}
+        />
+        <GoogleLoginButton
+          className="mt-3 mb-3"
+          onClick={() => this.handleGoogle()}
+        />
         <div className="text-center">
           <a href="/sign-up">Sign up</a>
           <span className="p-2">|</span>
@@ -37,6 +54,19 @@ class Authentication extends React.Component {
       </Form>
     );
   }
+
+  handleLogIn() {
+    alert("Log in");
+  }
+
+  handleFacebook() {
+    alert("Facebook");
+  }
+
+  handleGoogle() {
+    alert("Google");
+  }
+  //Login functionality will be replaced in the future
 }
 
 export default Authentication;
