ad73a6f797b4b56486a2a24e07f0fe5b0051e737
dustin-boettcher <dustin.boettcher@utexas.edu>
2021-04-09 15:17:40 -0500

    Revert "Update api.py"
    This reverts commit 36416294cecc0fc0f85d52d6d4e0c10e603989f2.

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import os
 
 
-app = Flask(__name__,static_folder='./build',static_url_path='/') 
+app = Flask(__name__) 
 
 #database setup
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///auth.db"
@@ -15,9 +15,7 @@ db = SQLAlchemy(app)
 
 api = Api(app)
 
-@app.route('/')
-def index():    #load page from react
-    return app.send_static_file('index.html')
+
 class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(20),unique=True,nullable=False)
@@ -61,7 +59,7 @@ class HardwareResources(Resource):
     def get(self, set_id, checkout, amount):
         # get database information for that hardware set
         entry = HWSets.query.all()
-        return jsonify([self.hardware_serialize(i) for i in entry])
+        return jsonify([*map(self.hardware_serialize, entry)])
 
     # function for requesting hardware resources. called when someone sends request for checkout/checkin
     # checkout is a T/F variable for checkout and checkin respectively
@@ -175,4 +173,4 @@ api.add_resource(Datasets, '/Datasets/<dataset_id>')
 api.add_resource(Login, '/Login/<username>/<password>')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=False,port=os.environ.get('PORT',80))
+    app.run(debug=True)
