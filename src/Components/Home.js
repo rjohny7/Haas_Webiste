802745ab013653ed194214ddf3b1b45b33a8e450
dustin-boettcher <dustin.boettcher@utexas.edu>
2021-05-03 20:24:31 -0500

    Added Descriptions to Homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -21,6 +21,46 @@ function Home() {
             </Typography>
           </CardContent>
         </Card>
+        <Card>
+          <CardContent>
+            <Typography className="hardware-header" variant={"h5"}>
+              Hardware Resources
+            </Typography>
+            <Typography className="hardware" variant={"body1"}>
+            You can view our hardware resources in the 'Our Hardware' tab. 
+            After signing in, you can checkout or checkin resources in the 'Computing Resources' tab. 
+            10 free credits will be awarded to your account upon signing up. 
+            Each credit gives you access to one hardware unit. 
+            Credits are returned upon checking in units. 
+            Note that you cannot check out more units than are available in each hardware set.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent>
+            <Typography variant={"h5"}>
+              Projects
+            </Typography>
+            <Typography variant={"body1"}>
+            You can create projects or manage existing ones by navigating to the 'Create or Manage Projects' tab. 
+            Here, you can create a project by entering a name, unique ID, and project description and clicking on 'Create Project'. 
+            In order to view an existing project, enter its unique ID and click 'View Project.'
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent>
+            <Typography variant={"h5"}>
+              Datasets
+            </Typography>
+            <Typography variant={"body1"}>
+            You can acess and download datasets by navigating to the 'Downloads' tab. 
+            Here, you can view all the datasets that are avialable along with their descriptions. 
+            Clicking on a dataset will download it as a zip file to your computer.
+            </Typography>
+            
+          </CardContent>
+        </Card>
       </div>
     </>
   );
